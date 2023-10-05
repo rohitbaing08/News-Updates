5731d9772b22a668154d03f9055c678806410e47
rohitbaing08 <rohitbaing456@gmail.com>
2023-10-05 23:10:21 +0530

    Ready to deploy

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -10,7 +10,7 @@ const Layout = () => {
   async function getData() {
     await axios
       .get(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=9cdd0be3853f479abbb78f6bb7b32e0e"
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
       )
       .then((res) => {
         if (res.data.status === "ok") {
@@ -27,7 +27,7 @@ const Layout = () => {
       <div className="layout">
         {data.map((item) => (
           <figure key={data.indexOf(item)} className="snip1347">
-            <img src={item.urlToImage} alt={`image${data.indexOf(item)}`} />
+            <img src={item.urlToImage} alt={''} />
             <div className="sizebox"></div>
             <figcaption>
               <h2>{item.title}</h2>
@@ -35,7 +35,7 @@ const Layout = () => {
                 <p>{`~ ${item.author}`}</p>
               </div>
               <p>{item.content}</p>
-              <a href={item.url} target="_blank" className="read-more">Read More</a>
+              <a href={item.url} target="_blank" rel="noreferrer" className="read-more">Read More</a>
             </figcaption>
           </figure>
         ))}
